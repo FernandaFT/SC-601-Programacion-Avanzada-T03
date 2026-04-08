02de92a5d774ae2a05fc2c6072c832c8dd8cb48f
Fernanda Fajardo <nanda199784@gmail.com>
2026-04-08 13:19:25 -0600

    Pantalla de bienvenida, css, js

---
+++ T03/T03/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace T03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region Consultar
+        [HttpGet]
+        public ActionResult Consultar()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Abonar Registro
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
